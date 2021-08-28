54aa07597575b0699771575888447c2a1286b6ce
nando <muhammad.rezki.ananda@gmail.com>
2021-08-28 08:55:31 +0700

    Setup endpoint (routes)

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,29 @@
-const routes = null;
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: () => {},
+  },
+];
 
 module.exports = routes;
