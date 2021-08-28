84e4f7f94e21207414abdd19eab1d5bb3610c752
nando <muhammad.rezki.ananda@gmail.com>
2021-08-29 01:11:21 +0700

    deleteDetailBookshelfByIdHanlder

--- src/handler.js
+++ src/handler.js
@@ -166,7 +166,26 @@ const updateDetailBookshelfByIdHanlder = (request, h) => {
 };
 
 const deleteDetailBookshelfByIdHanlder = (request, h) => {
+  const {bookId} = request.params;
 
+  const index = bookshelf.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    bookshelf.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
 };
 
 module.exports = {
