a3d1e94c314215445e11ec3c898064b750e1b706
nando <muhammad.rezki.ananda@gmail.com>
2021-08-29 01:08:43 +0700

    updateDetailBookshelfByIdHanlder

--- src/handler.js
+++ src/handler.js
@@ -107,7 +107,62 @@ const getDetailBookshelfByIdHanlder = (request, h) => {
 };
 
 const updateDetailBookshelfByIdHanlder = (request, h) => {
+  const {bookId} = request.params;
 
+  const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = bookshelf.findIndex((book) => book.id === bookId);
+
+  const noData = undefined;
+  const pageValid = (readPage > pageCount) ? false : true;
+
+  if (name === noData || name === {}) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+
+    response.code(400);
+
+    return response;
+  } else if (!(pageValid)) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+
+    response.code(400);
+
+    return response;
+  } else if (index !== -1) {
+    bookshelf[index] = {
+      ...bookshelf[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
 };
 
 const deleteDetailBookshelfByIdHanlder = (request, h) => {
